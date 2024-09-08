9d8c80f3eabb1384bde03b8d638a667422ae8561
sh1way <fhvygibi@gmail.com>
2024-09-08 21:07:53 +0300

    1 task

---
+++ lab1/lab1/lab1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int mass[10] = { 3, 2, 1, 4, 5, 10, 7, 8, 9, 6 };
+	int max = INT_MIN, min = INT_MAX;
+
+	for (int i = 0; i < 10; i++) {
+		if (mass[i] < min) {
+			min = mass[i];
+		}
+		if (mass[i] > max) {
+			max = mass[i];
+		}
+	}
+	
+	cout << "Max: " << max << endl;
+	cout << "Min: " << min << endl;
+
+	return 0;
+}
